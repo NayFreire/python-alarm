d65cc393c9b6e986954bb6274d6a1f1248b8fa16
NayFreire <nayfreireof@gmail.com>
2024-06-13 18:11:36 -0300

    Pygame example

---
+++ pygame_example/pygame_example.py
@@ -0,0 +1,14 @@
+import pygame as pg
+
+# Initializing the audio lib
+pg.mixer.init()
+
+# Loading the audio file
+pg.mixer.music.load('connor.mp3')
+
+# Play the audio
+pg.mixer.music.play()
+
+# Keep the script running while the audio plays
+while pg.mixer.music.get_busy(): 
+    pg.time.Clock().tick(3) # Verifying 3 times per second if the audio is still playing, so to not use cpu unnecessarily
